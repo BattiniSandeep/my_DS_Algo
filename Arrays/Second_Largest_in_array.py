75d6777823c7600b9dddbd87d3c0f62e382640a1
BattiniSandeep <76659791+BattiniSandeep@users.noreply.github.com>
2021-10-31 13:05:30 +0530

    Added comments for Second_Largest_in_array

--- Arrays/Second_Largest_in_array.py
+++ Arrays/Second_Largest_in_array.py
@@ -5,7 +5,7 @@
 def find_second_largest(arr):
 
     # Here we initialize 'result_index' as -1 because there might be a case where the second largest element is not there.
-    # for eg: arr: [55, 55, 55, 55, 55]. In this case our function should retun -1.
+    # for eg: arr: [55, 55, 55, 55, 55]. In this case our function should return -1.
     result_index = -1
     largest = 0
     for i in range(1, len(arr)):
@@ -15,9 +15,17 @@ def find_second_largest(arr):
             result_index = largest
             largest = i
         
-        # case2:  
+        # case2: If the elemnet is equal, we'll ignore it.
         elif arr[i] != arr[largest]:
+
+            # case 3: If the element is greater than the second highest and lesser than Largest element
+            # Then we'll update the 'result_index' to 'i'
             if result_index == -1 or arr[i] > arr[result_index]:
                 result_index = i
         
     return result_index
+
+arr = [3,1,5,2]
+
+# This should print 0(zero) as the second element is '3' which is at index 0(zero)
+print(find_second_largest(arr))
